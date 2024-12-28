75aaf5d59c9c9778d7b1d9857bc7c840e0777fa6
me26530 <61023195+me26530@users.noreply.github.com>
2024-12-28 22:48:57 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     # 示例URL和子域名对应的IP列表
     subdomain_ip_mapping = {
       #  'bestcf': 'https://ipdb.030101.xyz/api/bestcf.txt',  # #域名一，bestcf.域名.com
-        'cfip': 'https://raw.githubusercontent.com/me26530/youxuan/refs/heads/main/ip.txt', #域名二，api.域名.com
+        'cfip': 'https://raw.githubusercontent.com/me26530/youxuan/refs/heads/main/ip.txt?token=GHSAT0AAAAAAC2ZNY6PSUXOLX5JCNPOIX3EZ3QB5QQ', #域名二，api.域名.com
         # 添加更多子域名和对应的IP列表URL
     }
     
